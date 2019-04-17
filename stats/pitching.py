7f80feb5c7029fa4ce2e57788800018a220203a4
Brandon Lyons <brandontylerlyons@gmail.com>
2019-04-17 14:57:01 -0700

    Plot stike outs

--- stats/pitching.py
+++ stats/pitching.py
@@ -1,2 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from data import  games
+
+plays = games[games['type'] == 'play']
+
+strike_outs = plays[plays['event'].strn.contains('K')]
+strike_outs = strike_outs.groupby(['year', 'game_id']).size()
+strike_outs = strike_outs.reset_index(name='strike_outs')
+strike_outs = strike_outs.loc[:, ['year', 'strike_oouts']].apply(pd.to_numeric)
+strike_outs.plot(x='year', y='stike_outs', kind='scatter').legend(["strike Outs"])
+plt.show()
